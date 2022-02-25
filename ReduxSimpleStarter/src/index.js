6c51f8fc8a36c4881dde29495d80ce353dc7596d
ddyproject <98952351+ddyproject@users.noreply.github.com>
2022-02-25 23:00:42 +0300

    226 Render Targets

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+// 223 ES6 Import Statements
+// We are telling that we will use React component from the react library in node_modules folder.
+// Otherwise this will not be imported and we will get an error at React.render(App);
+import React from 'react'; // Core React knows how to render components, how to render them.
+import ReactDom from 'react-dom'; // taking the component and inserting into the dom is however is handeled by React Dom.
 
-import App from './components/app';
-import reducers from './reducers';
+// Create a new component
+// This componenet should produce some HTML
+// 222 More on JSX
+const App = () => { // => a new way to define function using ES6 syntax.
+    return <div>Hi!</div>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Take this component's generated HTML
+// and put it on the page (in the DOM)
+ReactDom.render(<App />, document.querySelector('.container')); // 223 ES6 Import Statements. // <App />: 224 ReactDOM vs React
+                        // , document.querySelector('.container'): says render this HTML from App, to the container (class name) element in our index.html
