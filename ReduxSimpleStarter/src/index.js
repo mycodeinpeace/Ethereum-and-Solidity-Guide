ba7eb12b3801f3103317a8821a3dad595b4484a3
ddyproject <98952351+ddyproject@users.noreply.github.com>
2022-02-26 22:23:39 +0300

    237 Refactoring Functional Components to Class Components: Start Code. Removed comments.

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,24 +1,14 @@
-// 223 ES6 Import Statements
-// We are telling that we will use React component from the react library in node_modules folder.
-// Otherwise this will not be imported and we will get an error at React.render(App);
 import React from 'react'; // Core React knows how to render components, how to render them.
 import ReactDom from 'react-dom'; // taking the component and inserting into the dom is however is handeled by React Dom.
-
-// 236 Youtube Search Response
-import YTSearch from 'youtube-api-search';
-
+import YTSearch from 'youtube-api-search'; // 236 Youtube Search Response
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyBP3Qmn8iXNUiLdH0SY3eVQiF8tO46fAoY'; // 228 Youtube Search API Signup
 
 // 236 Youtube Search Response
-// 
 YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
     console.log(data);
 });
 
-// Create a new component
-// This componenet should produce some HTML
 // 222 More on JSX
 const App = () => { // => a new way to define function using ES6 syntax.
     return (
@@ -28,8 +18,4 @@ const App = () => { // => a new way to define function using ES6 syntax.
     );
 }
 
-
-// Take this component's generated HTML
-// and put it on the page (in the DOM)
 ReactDom.render(<App />, document.querySelector('.container')); // 223 ES6 Import Statements. // <App />: 224 ReactDOM vs React
-                        // , document.querySelector('.container'): says render this HTML from App, to the container (class name) element in our index.html
