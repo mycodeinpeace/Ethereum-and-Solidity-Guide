ba79af476f15aee55607c96871e9b47fb2c13692
ddyproject <98952351+ddyproject@users.noreply.github.com>
2022-02-27 01:37:27 +0300

    239 Building Lists with Map

--- ReduxSimpleStarter/src/components/video_list.js
+++ ReduxSimpleStarter/src/components/video_list.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { pureFinalPropsSelectorFactory } from "react-redux/lib/connect/selectorFactory";
+import VideoListItem from "./video_list_item";
 
 const VideoList = (props) => {
+    const videoItems = props.videos.map((video) => {
+        return <VideoListItem video={video} />
+    });
+
     return (
         <ul className="col-md-4 list group">
-            
-            {props.videos.length}
+            {videoItems}
         </ul>
     );
 };
