4abf56a4589c84eff531ce7f3f9f5cf8b8bc8a4a
Code In Peace <me@codeinpeace.com>
2022-02-27 12:57:24 -0500

    80. Test Project Updates

--- Ethereum/Lottery/compile.js
+++ Ethereum/Lottery/compile.js
@@ -2,11 +2,11 @@ const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
 
-const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
-const source = fs.readFileSync(inboxPath, 'utf8');
+const lotteryPath = path.resolve(__dirname, 'contracts', 'Lottery.sol');
+const source = fs.readFileSync(lotteryPath, 'utf8');
 // Now we have the readFileSync
 
-module.exports = solc.compile(source, 1).contracts[':Inbox'];
+module.exports = solc.compile(source, 1).contracts[':Lottery'];
 // 1 says we are compiling only one contract.
 
 console.log(module.exports);
