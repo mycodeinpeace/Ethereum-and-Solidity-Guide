a0259691a08cb4e02d8f424aeea528cd12c30af6
Code In Peace <davut.deniz.yavuz@gmail.com>
2022-03-03 08:18:10 -0500

    Change Udemy's repo's lottery.js to use my deployed contract (abi,address)

--- EthereumCasts-master/lottery-react/src/lottery.js
+++ EthereumCasts-master/lottery-react/src/lottery.js
@@ -1,6 +1,6 @@
 import web3 from './web3';
 
-const address = '0x8DDB5D5F062C235A9eF6C65b789A3D3951134F0b';
+/*const address = '0x8DDB5D5F062C235A9eF6C65b789A3D3951134F0b';
 
 const abi = [
   {
@@ -54,6 +54,10 @@ const abi = [
     stateMutability: 'nonpayable',
     type: 'constructor'
   }
-];
+];*/
+
+const address = '0xdd673AA150Ce9A0c4167fE4f02BBD261Ea3B3A29';
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
+
 
 export default new web3.eth.Contract(abi, address);
